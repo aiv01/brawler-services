f8db7732992c41eb5bc6ee445786751fb002c5ba
Christian <christian.bomprezzi@outlook.com>
2017-03-31 11:36:38 +0200

    add players auth view

--- players/urls.py
+++ players/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
-from players.views import PlayerAlreadyExistsView, PlayerRegisterView, PlayerLoginView, PlayerPhotoView
+from players.views import PlayerAlreadyExistsView, PlayerRegisterView, PlayerLoginView, PlayerAuthView, PlayerPhotoView
 
 urlpatterns = [
     url(r'^already-exists/$', PlayerAlreadyExistsView.as_view(), name='player_already_exists'),
     url(r'^register/$', PlayerRegisterView.as_view(), name='player_register'),
     url(r'^login/$', PlayerLoginView.as_view(), name='player_login'),
+    url(r'^auth/$', PlayerAuthView.as_view(), name='player_auth'),
     url(r'^photo/$', PlayerPhotoView.as_view(), name='player_photo_audio'),
 ]
