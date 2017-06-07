de3dd44157adaad8da1e0af5e28cd4b554554a20
Christian <christian.bomprezzi@outlook.com>
2017-06-07 10:58:42 +0200

    modify match model

--- match/admin.py
+++ match/admin.py
@@ -4,17 +4,17 @@ from match.models import Match
 
 @admin.register(Match)
 class MatchAdmin(admin.ModelAdmin):
-    list_display = ('id', 'date', 'winner', 'get_participants')
-    search_fields = ('id', 'winner__username', 'participants__username')
-    date_hierarchy = 'date'
-    list_filter = (('winner', admin.RelatedOnlyFieldListFilter), )
+    list_display = ('id', 'start_date', 'end_date', 'winner', 'get_participants', 'server')
+    search_fields = ('id', 'winner__username', 'participants__username', 'server__ip', 'server__port', 'server__country')
+    date_hierarchy = 'start_date'
+    list_filter = (('winner', admin.RelatedOnlyFieldListFilter), ('server', admin.RelatedOnlyFieldListFilter), )
     fieldsets = (
-        ('Match', {'fields': (('id', 'date'), ), }),
+        ('Match', {'fields': (('id', 'server'), ('start_date', 'end_date'), ), }),
         ('Vincitore', {'fields': (('winner', ), ), }),
         ('Partecipanti', {'fields': (('participants', ), ), }), )
     filter_horizontal = ('participants', )
-    readonly_fields = ('id', 'date')
-    ordering = ['-date']
+    readonly_fields = ('id', 'start_date')
+    ordering = ['-start_date']
 
     def get_participants(self, model):
         return ", ".join([str(participant) for participant in model.participants.all()])
--- match/models.py
+++ match/models.py
@@ -1,9 +1,12 @@
 from django.db import models
+from servers.models import Server
 from players.models import Player
 
 
 class Match(models.Model):
-    date = models.DateTimeField('Data', auto_now_add=True)
+    start_date = models.DateTimeField('Start', auto_now_add=True)
+    end_date = models.DateTimeField('End', blank=True, null=True)
+    server = models.ForeignKey(Server, verbose_name='Server', blank=True, null=True)
     participants = models.ManyToManyField(Player, verbose_name='Partecipanti', related_name='match_participants', blank=True)
     winner = models.ForeignKey(Player, verbose_name='Vincitore', related_name='match_winner', blank=True, null=True)
 
