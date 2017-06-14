fe5a4ad0dd50a1fe7b5b4a5cbe1d5caaaeecb0c4
Christian <christian.bomprezzi@outlook.com>
2017-06-14 10:35:18 +0200

    update mobile match participants list view

--- mobile/views.py
+++ mobile/views.py
@@ -7,7 +7,22 @@ class MobileMatchParticipants(View):
 
     def get(self, request):
         match = Match.objects.filter(winner=None).last()
+        host = request.get_host()
+
         if match:
             participants = list(match.participants.all().values_list('username', flat=True))
-            return JsonResponse({'participants': participants})
+            participants_dict = {}
+
+            for participant in participants:
+
+                try:
+                    photo = match.participants.get(username=participant).photo.url
+                    photo = host + photo
+                except ValueError:
+                    photo = None
+
+                participants_dict[participant] = photo
+
+            return JsonResponse(participants_dict)
+
         return JsonResponse({'error': 'no match found'})
